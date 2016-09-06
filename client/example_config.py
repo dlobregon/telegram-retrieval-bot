bae6ff6c71f7fb6ccef2e0d8393fae15932c52b6
dlobregon <troyanrex@gmail.com>
2016-09-06 17:48:37 -0600

    se agrego los parametro de ejemplo de la ruta de conexion y el puerto en el archivo de configuracion

--- client/example_config.py
+++ client/example_config.py
@@ -1,3 +1,5 @@
 #aqui va el token de conexion hacia la plataforma de telegram
 #renombrar el archivo como "local_config.py"
 ConnectionToken='Telegram Connection Token'
+serverPath="your server connection url"
+serverPort="your server conection port"
